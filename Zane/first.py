4e28df2326e7bd2b513d5bcdba1034c4812f7ae2
ZaneS-25 <zane.Salam@oracle.com>
2025-02-03 22:52:49 +0000

    Added first.py

---
+++ Zane/first.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# Author: Zane Salam
+# Version: 1.0
+# Description: This is my first python script!
+
+# name="Zane Salam"
+name = input("Enter your name: ")
+
+print("My name is", name)
+print("My name is "+name+"!")
+
+import random
+print("The lucky number is",random.randint(1,100))
+
+import math
+print("Cosine of 0.5 is",math.cos(0.5))
+
---
+++ first.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# Author: Zane Salam
+# Version: 1.0
+# Description: This is my first python script!
+
+name="Zane Salam"
+
+print("My name is", name)
+print("My name is "+name+"!")
+
+import random
+print("The lucky number is",randint(1,100)
+
+import math
+print("Cosine of 0.5 is",cos(0.5)
+
