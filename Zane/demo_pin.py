ab071439c4ca3d75d5211b19cb51109cd31e69b3
ZaneS-25 <zane.Salam@oracle.com>
2025-02-04 19:55:07 +0000

    Updated demo_pin.py

--- Zane/demo_pin.py
+++ Zane/demo_pin.py
@@ -7,14 +7,25 @@
 
 master_pin = "0123"
 pin = None
+attempts = 0
+remaining_attempts = 0
+max_attempts = 3
 
 # Loop while PIN is incorrect
-while pin != master_pin:
+while pin != master_pin and attempts != max_attempts:
     pin = input("Enter PIN: ")
     if pin == master_pin:
         print("Valid PIN")
+        break
     else:
-        print("Invalid PIN")
+        attempts += 1
+        remaining_attempts = (max_attempts - attempts)
+
+        print("Invalid PIN,", remaining_attempts, "attempts remaining!")
+else:
+    # Executes ONLY ONCE when loop becomes false
+    print("Too many attempts")
+    print("The card is being shredded, alerting the authorities!")
 
 
 print("Done.")
