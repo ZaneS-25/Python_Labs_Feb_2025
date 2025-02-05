e9a401b9e0c4b0e10149a78001ccf147fed50309
ZaneS-25 <zane.Salam@oracle.com>
2025-02-05 01:06:48 +0000

    Added demo_str_formatting.py

---
+++ Zane/demo_str_formatting.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env/ python3
+# Author: Zane Salam
+# Description: This script will demo HOWTO format strings using str concatenation
+# and escape chars, str justification methods,
+#
+"""
+    Docstring:
+"""
